074e7312ebd52d52d5531c930a55eca4bfbd39d0
greenhornsr <srollins11279@gmail.com>
2019-04-18 20:47:10 -0700

    updated about page for phone layout..changes profile content size, adjusted frame width, changed display  orientation to column, swapped profile placard dots to individual images, updated constructors to work with dot2 profile images

--- components/About-Carousel/about2.js
+++ components/About-Carousel/about2.js
@@ -1,3 +1,4 @@
+// /*
 class DotCreator{
     constructor(dot){
         this.ourDots = dot;
@@ -8,7 +9,7 @@ class DotCreator{
         // console.log(this.profileElement);
 
         this.newProfile = new ProfileCreator(this.profileElement);
-        this.dotSelect = this.dotSelect.bind(this);
+        // this.dotSelect = this.dotSelect.bind(this);
         this.ourDots.addEventListener('click', () => {
             this.dotSelect();
         });
@@ -16,6 +17,9 @@ class DotCreator{
     // Methods
     dotSelect(){
         Array.from(allDots).forEach(function(dot){
+            dot.style.backgroundColor = 'white';
+        })
+        Array.from(allDots2).forEach(function(dot){
             dot.style.backgroundColor = 'white';
         })
         this.ourDots.style.backgroundColor = 'green';
@@ -42,5 +46,56 @@ allDots.forEach(function(dot){
     new DotCreator(dot);
     // dot.style.backgroundColor = 'green';
     // console.log(dot);
-})
+});
+const allDots2 = document.querySelectorAll('.dot2');
+allDots2.forEach(function(dot){
+    new DotCreator(dot);
+    // dot.style.backgroundColor = 'green';
+    // console.log(dot);
+});
 // console.log(allDots);
+// */
+
+// ARROWS!!
+// Arrow Functionality
+// class ControlArrows{
+//     constructor(element){
+//         this.arrow =  element;
+//         this.leftButton = this.document.querySelector('.left-button');
+//         this.rightButton = this.document.querySelector('.right-button');
+//         leftButton.addEventListener('click', () => {
+//             this.plusSlides(-1);
+//         });
+//         rightButton.addEventListener('click', () => {
+//             this.plusSlides(1);
+//         });
+//     }
+//     // Methods
+// }
+// // Establish baseline index;
+// let slideIndex = 1;
+// showSlides(slideIndex);
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("tmem2");
+//     let dots = document.getElementsByClassName("dot");
+//     if(n > slides.length) {slideIndex = 1} 
+//     if(n < 1) {slideIndex = slides.length}
+//     for(i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none"; 
+//     }
+//     // for (i = 0; i < dots.length; i++) {
+//     //     dots[i].className = dots[i].className.replace(" active", "");
+//     // }
+//     slides[slideIndex-1].style.display = "block"; 
+//     dots[slideIndex-1].style.backgroundColor = 'green';
+// }
